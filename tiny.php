be891fb6999ca03aac026421b4bec94c92f2e7e7
xshikata-ai <xshikata@gmail.com>
2025-11-03 21:39:59 +0700

    Update tiny.php

--- tiny.php
+++ tiny.php
@@ -1,6 +1,6 @@
 <?php
 //Default Configuration
-$CONFIG = '{"lang":"en","error_reporting":false,"show_hidden":true,"hide_Cols":false,"theme":"dark"}';
+$CONFIG = '{"lang":"en","error_reporting":false,"show_hidden":false,"hide_Cols":false,"theme":"dark"}';
 
 /**
  * H3K ~ Tiny File Manager V2.6
