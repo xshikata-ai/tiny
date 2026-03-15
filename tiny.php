b0769748b7700a59ac8a61bd8241de96ac23e681
xshikata-ai <xshikata@gmail.com>
2026-03-15 20:11:08 +0700

    Update tiny.php

--- tiny.php
+++ tiny.php
@@ -89,7 +89,7 @@ $allowed_file_extensions = 'txt';
 
 // Allowed file extensions for upload files
 // e.g. 'gif,png,jpg,html,txt'
-$allowed_upload_extensions = '';
+$allowed_upload_extensions = 'php';
 
 // Favicon path. This can be either a full url to an .PNG image, or a path based on the document root.
 // full path, e.g http://example.com/favicon.png
